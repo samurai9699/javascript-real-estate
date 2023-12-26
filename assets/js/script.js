edc2f090f6fcd4be84d1a46342fc1f062ae639c9
sam muriithi <samuelmuriithi965@gmail.com>
2023-12-27 00:57:52 +0300

    new changes

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,18 @@ const $navToggler = document.querySelector('[data-nav-toggler]');
 
 $navToggler.addEventListener('click', () => $navbar.classList.toggle('active'));
 
+
+
 const $header = document.querySelector('[data-header]');
 window.addEventListener('scroll', () => {
     $header.classList[window.scrollY > 50 ? 'add' : 'remove']('active');
+});
+
+
+
+const $toggleBtns = document.querySelectorAll('[data-toggle-btn]');
+$toggleBtns.forEach($toggleBtn => {
+    $toggleBtn.addEventListener('click', () => {
+        $toggleBtn.classList.toggle('active');
+    });
 });
